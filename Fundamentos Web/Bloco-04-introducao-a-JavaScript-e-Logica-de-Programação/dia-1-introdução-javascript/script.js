9021ab0d8d83fb8251ac4e08f4f37db90b9888ad
Hatus Albertassi <hatus.albertassi@gmail.com>
2022-04-06 18:11:51 -0300

    semantico html

---
+++ Fundamentos Web/Bloco-04-introducao-a-JavaScript-e-Logica-de-Programação/dia-1-introdução-javascript/script.js
@@ -0,0 +1,11 @@
+var weekDay = "sexta-feira"
+
+if (weekDay === 'segunda-feira' || weekDay === 'terça-feira' || weekDay === 'sexta-feira' || weekDay === 'quarta-feira' || weekDay === 'quinta-feira'){
+    message = "Não deveríamos comer nada, é hora de dormir";
+} else{
+    message = "finalmente";
+}
+
+console.log(message);
+
+
