c58ed7bde80bc57329e9a4a2dbfdc566754c6d7a
Hatus Albertassi <hatus.albertassi@gmail.com>
2022-04-26 19:15:35 -0300

    adc elementos css

--- Fundamentos Web/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ Fundamentos Web/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,14 +1,43 @@
-const submitButton = document.querySelector('#send');
-const eraserButton = document.querySelector('#eraser');
 
-function getSubmit(event){
-    event.preventDefault(); 
+// const eraserButton = document.querySelector('#eraser');
+
+
+
+
+
+function verify(){
+    const name = document.querySelector('#input-text').value.length;
+    const invalidName = name < 10 || name > 40;
+
+    const email = document.querySelector('#input-email').value.length;
+    const invalidEmail = email < 10 || email > 50;
+
+    const about = document.querySelector('#text-area1').value.length;
+    const invalidAbout = about >= 500;
+
+    if (invalidName || invalidEmail || invalidAbout){
+        return false;
+    }
+    else {
+        return true;
+    }
 };
 
-// function getEraser(event){
-//     event.preventDefault(); 
-//     event.reload();
-// };
+const submitButton = document.querySelector('#send');
 
+function getSubmit(event){
+
+    
+
+    event.preventDefault(); 
+
+    const verification = verify();
+    if (verification === false){
+        alert("Dados Inválidos");
+    }
+    else {
+       alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
+    }
+    
+};
 submitButton.addEventListener('click', getSubmit);
-// eraserButton.addEventListener('click', getEraser);
