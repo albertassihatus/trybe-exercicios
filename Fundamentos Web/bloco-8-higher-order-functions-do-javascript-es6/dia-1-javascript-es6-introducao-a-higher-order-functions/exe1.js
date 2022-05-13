52c7991c89088c7e829bdcca59a04abf0e8e5144
Hatus Albertassi <hatus.albertassi@gmail.com>
2022-05-13 19:15:14 -0300

    exercicio 1

--- Fundamentos Web/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exe1.js
+++ Fundamentos Web/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exe1.js
@@ -1,18 +1,19 @@
-const user = (nomeCompleto) => {
-    const eMail = nomeCompleto.toLowerCase().replace(' ', '_');
-    return {
-      nomeCompleto: nomeCompleto,
-      email: `${eMail}@trybe.com`
-    };
+const newEmployees = (callback) => {
+  const employees = {
+    id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
   }
-  
-  const newEmployees = (callback) => {
-    const employees = {
-      id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-      id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-      id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
-    }
-    return employees;
+  return employees;
+}
+
+const gerarEmail = (nomeCompleto) => {
+  const emailNovo = `${nomeCompleto.replace(' ','_').toLowerCase()}@trybe.com`;
+
+  return {
+    fullName: nomeCompleto,
+    email: emailNovo,
   };
-  
-  console.log(newEmployees(user));
+}
+
+console.log(newEmployees(gerarEmail));
